2519ee7673c29f354669913005a5e8bff69b33d1
Zechary <zechary@datature-io>
2022-05-21 16:05:28 +0800

    Change error handling to use custom errors

--- src/models/users.db.ts
+++ src/models/users.db.ts
@@ -1,9 +1,7 @@
 import { PrismaClient, Prisma } from "@prisma/client";
 import { PrismaClientKnownRequestError } from "@prisma/client/runtime";
-import {
-  unknownInternalServerError,
-  userEmailNotFoundError,
-} from "src/utils/errors";
+import { SkylabError } from "src/errors/SkylabError";
+import { HttpStatusCode } from "src/utils/HTTP_Status_Codes";
 
 const prisma = new PrismaClient();
 
@@ -27,8 +25,21 @@ export interface IUser {
 export const createUser = async (
   userToCreate: Prisma.UserCreateInput | IUser
 ) => {
-  const createUser = await prisma.user.create({ data: userToCreate });
-  return createUser;
+  try {
+    const createUser = await prisma.user.create({ data: userToCreate });
+    return createUser;
+  } catch (e) {
+    if (!(e instanceof PrismaClientKnownRequestError)) {
+      throw e;
+    }
+
+    // Unique constraint error
+    if (e.code === "P2002") {
+      throw new SkylabError("User is not unique", HttpStatusCode.BAD_REQUEST);
+    }
+
+    throw new SkylabError(e.message, HttpStatusCode.BAD_REQUEST);
+  }
 };
 
 /**
@@ -51,10 +62,13 @@ export const createManyUsers = async (
     }
 
     if (e.code === "P2002") {
-      throw new Error("User data has conflicting unique inputs");
+      throw new SkylabError(
+        "At least one user is not unique",
+        HttpStatusCode.BAD_REQUEST
+      );
     }
 
-    throw e;
+    throw new SkylabError(e.message, HttpStatusCode.BAD_REQUEST);
   }
 };
 
@@ -85,19 +99,18 @@ export const getUsers = async (searchCriteria: { [key: string]: string }) => {
  * @returns User that has the given email
  */
 export const getUserByEmail = async (email: string) => {
-  try {
-    const user = await prisma.user.findUnique({
-      where: { email: email },
-    });
+  const user = await prisma.user.findUnique({
+    where: { email: email },
+  });
 
-    if (user == null) {
-      throw new Error("User not found");
-    }
-
-    return user;
-  } catch (e) {
-    throw e;
+  if (user == null) {
+    throw new SkylabError(
+      "User with given email was not found",
+      HttpStatusCode.BAD_REQUEST
+    );
   }
+
+  return user;
 };
 
 export const updateUserByEmail = async (
@@ -118,10 +131,13 @@ export const updateUserByEmail = async (
     }
 
     if (e.code === "P2025") {
-      throw userEmailNotFoundError;
+      throw new SkylabError(
+        "User with given email was not found",
+        HttpStatusCode.BAD_REQUEST
+      );
     }
 
-    throw unknownInternalServerError;
+    throw new SkylabError(e.message, HttpStatusCode.BAD_REQUEST);
   }
 };
 
@@ -144,9 +160,12 @@ export const deleteUserByEmail = async (email: string) => {
     }
 
     if (e.code === "P2025") {
-      throw userEmailNotFoundError;
+      throw new SkylabError(
+        "User with given email was not found",
+        HttpStatusCode.BAD_REQUEST
+      );
     }
 
-    throw unknownInternalServerError;
+    throw new SkylabError(e.message, HttpStatusCode.BAD_REQUEST);
   }
 };
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,4 +1,5 @@
 import { Router, Request, Response } from "express";
+import { SkylabError } from "src/errors/SkylabError";
 import {
   createManyUsers,
   createUser,
@@ -16,13 +17,19 @@ router
   .post("/", async (req: Request, res: Response) => {
     try {
       if (!req.body.user) {
-        throw apiCallArgumentsMissingError;
+        res
+          .status(HttpStatusCode.BAD_REQUEST)
+          .send("Arguments missing from request");
       }
 
       await createUser(req.body.user);
       res.sendStatus(HttpStatusCode.OK);
     } catch (e) {
-      res.status(HttpStatusCode.INTERNAL_SERVER_ERROR).send(e.message);
+      if (!(e instanceof SkylabError)) {
+        res.status(HttpStatusCode.INTERNAL_SERVER_ERROR).send(e.message);
+      } else {
+        res.status(e.statusCode).send(e.message);
+      }
     }
   })
   .get("/", async (_: Request, res: Response) => {
@@ -30,7 +37,11 @@ router
       const allUsers = await getAllUsers();
       res.status(HttpStatusCode.OK).json(allUsers);
     } catch (e) {
-      res.status(HttpStatusCode.INTERNAL_SERVER_ERROR).send(e.message);
+      if (!(e instanceof SkylabError)) {
+        res.status(HttpStatusCode.INTERNAL_SERVER_ERROR).send(e.message);
+      } else {
+        res.status(e.statusCode).send(e.message);
+      }
     }
   })
   .all("/", (_: Request, res: Response) => {
@@ -59,7 +70,11 @@ router
         .status(HttpStatusCode.OK)
         .send(`${createAllUsers} users were created`);
     } catch (e) {
-      res.status(HttpStatusCode.INTERNAL_SERVER_ERROR).send(e.message);
+      if (!(e instanceof SkylabError)) {
+        res.status(HttpStatusCode.INTERNAL_SERVER_ERROR).send(e.message);
+      } else {
+        res.status(e.statusCode).send(e.message);
+      }
     }
   })
   .all("/batch", (_: Request, res: Response) => {
@@ -72,14 +87,20 @@ router
   .get("/:email", async (req: Request, res: Response) => {
     try {
       if (!req.params.email) {
-        throw apiCallArgumentsMissingError;
+        res
+          .status(HttpStatusCode.BAD_REQUEST)
+          .send("Arguments missing from request");
       }
 
       const email = req.params.email;
       const user = await getUserByEmail(email);
       res.status(HttpStatusCode.OK).json(user);
     } catch (e) {
-      res.status(HttpStatusCode.INTERNAL_SERVER_ERROR).send(e.message);
+      if (!(e instanceof SkylabError)) {
+        res.status(HttpStatusCode.INTERNAL_SERVER_ERROR).send(e.message);
+      } else {
+        res.status(e.statusCode).send(e.message);
+      }
     }
   })
   .delete("/:email", async (req: Request, res: Response) => {
@@ -94,18 +115,32 @@ router
       await deleteUserByEmail(email);
       res.sendStatus(HttpStatusCode.OK);
     } catch (e) {
-      res.status(HttpStatusCode.INTERNAL_SERVER_ERROR).send(e.message);
+      if (!(e instanceof SkylabError)) {
+        res.status(HttpStatusCode.INTERNAL_SERVER_ERROR).send(e.message);
+      } else {
+        res.status(e.statusCode).send(e.message);
+      }
     }
   })
   .put("/:email", async (req: Request, res: Response) => {
-    if (!req.params.email || !req.body.user) {
-      res.status(HttpStatusCode.BAD_REQUEST).send("Missing request parameters");
-    }
+    try {
+      if (!req.params.email || !req.body.user) {
+        res
+          .status(HttpStatusCode.BAD_REQUEST)
+          .send("Missing request parameters");
+      }
 
-    const email = req.params.email;
-    const user = req.body.user;
-    await updateUserByEmail(email, user);
-    res.sendStatus(HttpStatusCode.OK);
+      const email = req.params.email;
+      const user = req.body.user;
+      await updateUserByEmail(email, user);
+      res.sendStatus(HttpStatusCode.OK);
+    } catch (e) {
+      if (!(e instanceof SkylabError)) {
+        res.status(HttpStatusCode.INTERNAL_SERVER_ERROR).send(e.message);
+      } else {
+        res.status(e.statusCode).send(e.message);
+      }
+    }
   })
   .all("/:email", (_: Request, res: Response) => {
     res
