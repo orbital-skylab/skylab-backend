0564da5b90c398d4deff039b72419d2f50fac48e
Zechary <zechary@datature-io>
2022-06-04 19:51:59 +0800

    Cohort helper file docs

--- src/helpers/cohorts.helper.ts
+++ src/helpers/cohorts.helper.ts
@@ -1,9 +1,17 @@
 import { getFirstCohort, getManyCohorts } from "src/models/cohorts.db";
 
+/**
+ * @function getLatestCohort Get the most recent cohort
+ * @returns The most recent cohort record by academicYear
+ */
 export const getLatestCohort = async () => {
   return await getFirstCohort({ orderBy: { academicYear: "desc" } });
 };
 
+/**
+ * @function getAllCohorts Get all the cohorts in the database
+ * @returns All the cohort records in the database
+ */
 export const getAllCohorts = async () => {
   return await getManyCohorts({});
 };
