5ca6d65426aa18d490e6bcdc2f2d82afcbddb668
myc37 <nvjn37@gmail.com>
2022-06-19 22:31:11 +0800

    feat: add user info route

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -3,6 +3,7 @@ import { SkylabError } from "src/errors/SkylabError";
 import { userLogin } from "src/helpers/users.helper";
 import authorize from "src/middleware/jwtAuth";
 import { getOneUserWithRoleData } from "src/models/users.db";
+import { apiResponseWrapper } from "src/utils/ApiResponseWrapper";
 import { HttpStatusCode } from "src/utils/HTTP_Status_Codes";
 
 const router = Router();
@@ -12,15 +13,26 @@ router.post("/sign-in", async (req: Request, res: Response) => {
     const { email, password } = req.body;
 
     if (!email || !password) {
-      res.status(HttpStatusCode.BAD_REQUEST).send("Missing request parameters");
+      return apiResponseWrapper(
+        res,
+        {},
+        new SkylabError(
+          "Missing request parameters",
+          HttpStatusCode.BAD_REQUEST
+        )
+      );
     }
 
     const { token } = await userLogin(email, password);
+    console.log("token: ", token);
+
     if (token) {
-      const userData = await getOneUserWithRoleData(email);
+      const userData = await getOneUserWithRoleData({
+        where: { email: email },
+      });
       res
         .status(HttpStatusCode.OK)
-        .cookie("jwt", token, { httpOnly: true })
+        .cookie("token", token, { httpOnly: true })
         .json(userData);
     } else {
       res.status(HttpStatusCode.UNAUTHORIZED).send("Password is incorrect");
@@ -34,12 +46,12 @@ router.post("/sign-in", async (req: Request, res: Response) => {
   }
 });
 
-router.post(
+router.get(
   "/:userId/sign-out",
   authorize,
   async (_: Request, res: Response) => {
     try {
-      res.status(HttpStatusCode.OK).clearCookie("jwt");
+      res.clearCookie("token").sendStatus(HttpStatusCode.OK);
     } catch (e) {
       if (!(e instanceof SkylabError)) {
         res.status(HttpStatusCode.INTERNAL_SERVER_ERROR).send(e.message);
@@ -49,5 +61,21 @@ router.post(
     }
   }
 );
+
+router.get("/:userId/info", authorize, async (req: Request, res: Response) => {
+  try {
+    const { userId } = req.params;
+    const userData = await getOneUserWithRoleData({
+      where: { id: Number(userId) },
+    });
+    res.status(HttpStatusCode.OK).json(userData);
+  } catch (e) {
+    if (!(e instanceof SkylabError)) {
+      res.status(HttpStatusCode.INTERNAL_SERVER_ERROR).send(e.message);
+    } else {
+      res.status(e.statusCode).send(e.message);
+    }
+  }
+});
 
 export default router;
