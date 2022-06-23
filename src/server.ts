93ff34cd8c65e04d081b86aa62680582200c2b45
myc37 <nvjn37@gmail.com>
2022-06-23 10:18:47 +0800

    feat: update cors to allow http session

--- src/server.ts
+++ src/server.ts
@@ -20,7 +20,25 @@ app.disable("x-powered-by");
  **********************************************************************************/
 
 // Common middlewares
-app.use(cors());
+const allowedOrigins = [
+  "https://skylab-frontend.vercel.app/",
+  "http://localhost:3000/",
+];
+app.use(
+  cors({
+    credentials: true,
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const msg =
+          "The CORS policy for this site does not " +
+          "allow access from the specified Origin.";
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
