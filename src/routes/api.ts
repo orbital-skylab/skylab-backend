7b56aafe7f1adb55e5d3461f86356b76a0faa928
N Vijay Narayanan <nvjn37@gmail.com>
2022-06-26 17:04:34 +0800

    feat: switch admin router to dev router

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -7,7 +7,7 @@ import cohortRouter from "./cohorts";
 import projectRouter from "./projects";
 import adviserRouter from "./advisers";
 import facilitatorRouter from "./facilitators";
-import adminRouter from "./admin";
+import devRouter from "./dev";
 import deadlineRouter from "./deadlines";
 import authRouter from "./auth";
 
@@ -22,7 +22,7 @@ baseRouter.use("/cohorts", cohortRouter);
 baseRouter.use("/projects", projectRouter);
 baseRouter.use("/advisers", adviserRouter);
 baseRouter.use("/facilitators", facilitatorRouter);
-baseRouter.use("/admin", adminRouter);
+baseRouter.use("/admin", devRouter);
 baseRouter.use("/deadlines", deadlineRouter);
 baseRouter.use("/auth", authRouter);
 
