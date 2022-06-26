3a611c4316ad960fc8147d166178cdd5529d8e29
N Vijay Narayanan <nvjn37@gmail.com>
2022-06-27 03:09:09 +0800

    feat: add administrator router

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -10,6 +10,7 @@ import facilitatorRouter from "./facilitators";
 import devRouter from "./dev";
 import deadlineRouter from "./deadlines";
 import authRouter from "./auth";
+import administratorRouter from "./administrators";
 
 // Export the base-router
 const baseRouter = Router();
@@ -25,6 +26,7 @@ baseRouter.use("/facilitators", facilitatorRouter);
 baseRouter.use("/dev", devRouter);
 baseRouter.use("/deadlines", deadlineRouter);
 baseRouter.use("/auth", authRouter);
+baseRouter.use("/administrators", administratorRouter);
 
 // Export default.
 export default baseRouter;
