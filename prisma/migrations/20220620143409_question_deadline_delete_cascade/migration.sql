29f5bcffdecd3d9db7af269be469013ebfc89e9d
Zechary <zechary@datature-io>
2022-07-17 21:15:21 +0800

    Squash previous migrations

--- prisma/migrations/20220620143409_question_deadline_delete_cascade/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropForeignKey
-ALTER TABLE "Question" DROP CONSTRAINT "Question_deadlineId_fkey";
-
--- AddForeignKey
-ALTER TABLE "Question" ADD CONSTRAINT "Question_deadlineId_fkey" FOREIGN KEY ("deadlineId") REFERENCES "Deadline"("id") ON DELETE CASCADE ON UPDATE CASCADE;
--- prisma/migrations/20220704073120_anon_to_is_anon/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `anonymous` on the `Question` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Question" DROP COLUMN "anonymous",
-ADD COLUMN     "isAnonymous" BOOLEAN NOT NULL DEFAULT false;
