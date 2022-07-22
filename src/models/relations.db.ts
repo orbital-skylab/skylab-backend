f94941b93a0b077ff29cef71516c9a408e28896b
Zechary <zechary@datature-io>
2022-07-23 00:28:30 +0800

    Create many evaluation relations by group endpoint

--- src/models/relations.db.ts
+++ src/models/relations.db.ts
@@ -1,10 +1,34 @@
 import { Prisma } from "@prisma/client";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime";
+import { SkylabError } from "src/errors/SkylabError";
+import { HttpStatusCode } from "src/utils/HTTP_Status_Codes";
 import { prisma } from "../client";
 
 export async function createOneRelation(
   relation: Prisma.EvaluationRelationCreateArgs
 ) {
   return await prisma.evaluationRelation.create(relation);
+}
+
+export async function createManyRelations(
+  relations: Prisma.EvaluationRelationCreateManyArgs
+) {
+  try {
+    return await prisma.evaluationRelation.createMany(relations);
+  } catch (e) {
+    if (!(e instanceof PrismaClientKnownRequestError)) {
+      throw e;
+    }
+
+    if (e.code === "P2002") {
+      throw new SkylabError(
+        "Creating Evaluation Relations for group failed: One of the relations already exist",
+        HttpStatusCode.BAD_REQUEST
+      );
+    }
+
+    throw e;
+  }
 }
 
 export async function updateOneRelation(
@@ -22,7 +46,10 @@ export async function findUniqueRelation(
 export async function findManyRelations(
   query: Prisma.EvaluationRelationFindManyArgs
 ) {
-  return await prisma.evaluationRelation.findMany(query);
+  return await prisma.evaluationRelation.findMany({
+    ...query,
+    orderBy: { fromProjectId: "asc", toProjectId: "asc" },
+  });
 }
 
 export async function deleteOneRelation(
