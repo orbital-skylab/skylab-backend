9fd7e111edb616171e20fdecdbfaa469206eeb97
Zechary <zechary@datature-io>
2022-07-23 22:56:24 +0800

    Split seed routes into separate endpoints

--- src/routes/seed.ts
+++ src/routes/seed.ts
@@ -1,5 +1,11 @@
 import { Router, Request, Response } from "express";
+import { seedAdmins } from "src/seed/administrator.seed";
+import { seedAdvisers } from "src/seed/adviser.seed";
+import { seedCohorts } from "src/seed/cohort.seed";
+import { seedDeadlines } from "src/seed/deadline.seed";
+import { seedMentors } from "src/seed/mentor.seed";
 import { seedAll } from "src/seed/seed.index";
+import { seedStudents } from "src/seed/student.seed";
 import {
   apiResponseWrapper,
   routeErrorHandler,
@@ -10,6 +16,54 @@ const router = Router();
 router.post("/", async (_: Request, res: Response) => {
   try {
     await seedAll();
+    return apiResponseWrapper(res, { response: "Donezo" });
+  } catch (e) {
+    return routeErrorHandler(res, e);
+  }
+});
+router.post("/cohorts", async (_: Request, res: Response) => {
+  try {
+    await seedCohorts();
+    return apiResponseWrapper(res, { response: "Donezo" });
+  } catch (e) {
+    return routeErrorHandler(res, e);
+  }
+});
+router.post("/students", async (_: Request, res: Response) => {
+  try {
+    await seedStudents();
+    return apiResponseWrapper(res, { response: "Donezo" });
+  } catch (e) {
+    return routeErrorHandler(res, e);
+  }
+});
+router.post("/mentors", async (_: Request, res: Response) => {
+  try {
+    await seedMentors();
+    return apiResponseWrapper(res, { response: "Donezo" });
+  } catch (e) {
+    return routeErrorHandler(res, e);
+  }
+});
+router.post("/advisers", async (_: Request, res: Response) => {
+  try {
+    await seedAdvisers();
+    return apiResponseWrapper(res, { response: "Donezo" });
+  } catch (e) {
+    return routeErrorHandler(res, e);
+  }
+});
+router.post("/admins", async (_: Request, res: Response) => {
+  try {
+    await seedAdmins();
+    return apiResponseWrapper(res, { response: "Donezo" });
+  } catch (e) {
+    return routeErrorHandler(res, e);
+  }
+});
+router.post("/deadlines", async (_: Request, res: Response) => {
+  try {
+    await seedDeadlines();
     return apiResponseWrapper(res, { response: "Donezo" });
   } catch (e) {
     return routeErrorHandler(res, e);
