1b64f594f85f1930175a928ad8ccd4ce3db79e40
Zechary <zechary@datature-io>
2022-07-28 20:56:50 +0800

    Add sections to submission data, fix isDraft update bug

--- src/helpers/submissions.helper.ts
+++ src/helpers/submissions.helper.ts
@@ -1,16 +1,45 @@
 import { Answer } from "@prisma/client";
+import { SkylabError } from "src/errors/SkylabError";
 import { createUniqueAnswer, deleteManyAnswers } from "src/models/answers.db";
+import { findUniqueDeadlineWithQuestionsData } from "src/models/deadline.db";
 import {
   createUniqueSubmission,
   findUniqueSubmission,
   updateUniqueSubmission,
 } from "src/models/submissions.db";
+import { HttpStatusCode } from "src/utils/HTTP_Status_Codes";
 
 export async function getSubmissionBySubmissionId(submissionId: number) {
-  return await findUniqueSubmission({
+  const submission = await findUniqueSubmission({
     where: { id: submissionId },
-    include: { answers: { include: { question: true } } },
+    include: {
+      answers: { include: { question: true } },
+      fromProject: true,
+      fromUser: true,
+      toProject: true,
+      toUser: true,
+    },
   });
+
+  if (!submission) {
+    throw new SkylabError(
+      "Submission with this ID was not found",
+      HttpStatusCode.BAD_REQUEST
+    );
+  }
+
+  const { deadlineId } = submission;
+  const deadlineWithQuestions = await findUniqueDeadlineWithQuestionsData({
+    where: { id: deadlineId },
+  });
+
+  const { sections, ...deadlineData } = deadlineWithQuestions;
+
+  return {
+    ...submission,
+    deadline: deadlineData,
+    sections: sections,
+  };
 }
 
 export async function createOneSubmission(body: {
@@ -87,13 +116,12 @@ export async function updateOneSubmissionBySubmissionId(
     );
   }
 
-  if (isDraft) {
-    return await updateUniqueSubmission({
+  if (typeof isDraft !== "undefined") {
+    await updateUniqueSubmission({
       where: { id: submissionId },
       data: { isDraft: isDraft },
-      include: { answers: true },
     });
-  } else {
-    return await findUniqueSubmission({ where: { id: submissionId } });
   }
+
+  return await findUniqueSubmission({ where: { id: submissionId } });
 }
