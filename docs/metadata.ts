bc6a3b441c80f7803aaa78a5276524e108ab08f2
grenn24 <Hoodiheng@gmail.com>
2026-01-25 01:38:06 -0500

    Modify erd output dir

---
+++ docs/metadata.ts
@@ -0,0 +1,19 @@
+const metadata = [
+  {
+    file: "levels-of-achievement.md",
+    namespace: "core",
+    url: "https://www.comp.nus.edu.sg/orbital/2020/04/16/levels-of-achievement/",
+  },
+  {
+    file: "programme-structure.md",
+    namespace: "core",
+    url: "https://www.comp.nus.edu.sg/orbital/",
+  },
+  {
+    file: "mentorship.md",
+    namespace: "core",
+    url: "https://www.comp.nus.edu.sg/orbital/mentorship/",
+  },
+];
+
+export default metadata;
