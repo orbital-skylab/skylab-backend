30511d64275e3a6ceb76ef53b161e5f64fd6788f
sharonsohxuanhui@hotmail.com <sharonsohxuanhui@hotmail.com>
2026-03-04 20:13:26 +0800

    fix: undefined team name for delete relations via team

--- src/helpers/projects.helper.ts
+++ src/helpers/projects.helper.ts
@@ -121,7 +121,7 @@ export async function getManyProjectsLean(
       cohortYear: cohortYear,
       hasDropped: hasDropped,
     },
-    select: { id: true, name: true },
+    select: { id: true, name: true, teamName: true },
     orderBy: { id: "asc" },
   });
   return leanProjects;
