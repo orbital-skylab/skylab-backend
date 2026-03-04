c8f4d8fa5f12d11b79e9733ab7b734a4bd675c87
sharonsohxuanhui@hotmail.com <sharonsohxuanhui@hotmail.com>
2026-03-04 20:33:23 +0800

    fix: refactor code for ordering asc for evaluations deadline

--- src/helpers/dashboard.admin.helper.ts
+++ src/helpers/dashboard.admin.helper.ts
@@ -156,7 +156,6 @@ export const getAllSubmissions = async (
     },
     take: query.limit ?? undefined,
     skip: query.limit && query.page ? limit * page : undefined,
-    orderBy: { id: "asc" },
   });
 
   const milestoneDeadlines = await findManyDeadlines({
