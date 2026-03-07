258405f55dc74bfd860c2498839ef664eecf865b
sharonsohxuanhui@hotmail.com <sharonsohxuanhui@hotmail.com>
2026-03-07 18:29:02 +0800

    fix: fix bug by removing migration edit and create a new migration file instead

--- prisma/migrations/20220723142219_squash_migrations/migration.sql
+++ prisma/migrations/20220723142219_squash_migrations/migration.sql
@@ -133,7 +133,6 @@ CREATE TABLE "Question" (
     "desc" TEXT NOT NULL,
     "type" "QuestionType" NOT NULL,
     "isAnonymous" BOOLEAN NOT NULL DEFAULT false,
-    "isRequired" BOOLEAN NOT NULL DEFAULT false,
     "sectionId" INTEGER NOT NULL,
 
     CONSTRAINT "Question_pkey" PRIMARY KEY ("id")
